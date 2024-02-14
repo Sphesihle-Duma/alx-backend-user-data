5ede5de299bb02f8927ab7e59ef61ea5cee8ca41
Your Name <you@example.com>
2024-02-14 10:20:30 -0800

    validation of requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,10 +16,25 @@ auth = None
 if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-    
+
 
 @app.before_request
-def handle_before_request()
+def handle_before_request():
+    '''
+       validating request
+       arguements
+          list of request to test against
+    '''
+    print(request)
+    if auth is not None:
+        list_of_routes = ['/api/v1/status/', '/api/v1/unauthorized/',
+                             '/api/v1/forbidden/']
+        request_path = request.path
+        if auth.require_auth(request_path, list_of_routes):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
