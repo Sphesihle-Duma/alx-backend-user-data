5d5c67ec66ae659f69a679d59618799ed1a51eff
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-14 21:05:36 +0200

    changing request.header to request.headers in Authorization method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,9 +29,9 @@ class Auth:
         '''
         if request == None:
             return None
-        if 'Authorization' not in request.header:
+        if 'Authorization' not in request.headers:
             return None
-        return request.header['Authorization']
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         ''' The current user to be authorized
