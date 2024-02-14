d0702f46625d05906cfd847a95789cd4da6a492d
Your Name <you@example.com>
2024-02-14 00:21:44 -0800

    Added the auth class

--- 0x01-Basic_authentication/api/v1/auth/__init__.py
+++ 0x01-Basic_authentication/api/v1/auth/__init__.py
@@ -1,24 +1,3 @@
-from flask import request
-''' authentication module
-'''
 
-
-class Auth:
-    '''
-       authentication class
-    '''
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''
-           public class for authentication
-        '''
-        return False
-
-
-    def authorization_header(self, request=None) -> str:
-        '''
-           authorization header function
-        '''
-        return None
 
     
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+from flask import request
+from typing import List, TypeVar
+'''
+   Authentication module
+'''
+
+
+class Auth:
+    ''' Authentication class
+
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require authorization function
+        '''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''Authorization function
+        '''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' The current user to be authorized
+        '''
+        return None
