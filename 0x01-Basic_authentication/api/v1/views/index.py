6160ea8c5ae6aae5cfd3e067778d531568ffb512
Your Name <you@example.com>
2024-02-13 22:54:21 -0800

    adding strict_slashes parameter on the forbidden route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,7 +33,7 @@ def unauthorized():
     abort(401)
 
 
-@app_views.route('/forbidden')
+@app_views.route('/forbidden', strict_slashes=False)
 def forbidden():
     ''' forbidden view function
     '''
