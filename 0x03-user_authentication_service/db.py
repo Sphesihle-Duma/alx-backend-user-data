f2dfc6fe620b5c3d4a63fe3e3f7e2a94e0d7a20e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-19 22:16:10 +0200

    added find_user function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -44,3 +46,15 @@ class DB():
         new_session.add(user)
         new_session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        '''
+          finding the user using keywords
+        '''
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
