98616e3bfd101ff1390a3408cc569ef8ce206e4f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-23 23:47:34 +0200

    destroying the session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,3 +90,9 @@ class Auth:
             return None
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        ''' Destroying a session
+        '''
+        if user_id and isinstance(user_id, int):
+            self._db.update_user(user_id, session_id=None)
