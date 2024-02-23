76bd41a11bf05383725b1f9ca44cd13b6d9bcd01
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-23 22:10:46 +0200

    creating a session for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,19 @@ class Auth:
             return bcrypt.checkpw(b_passwd, user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        '''
+           creating a session for a user
+           Args:
+              email: user's email
+           return:
+              session id
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
